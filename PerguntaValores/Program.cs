094c6a2e9baf531fa22fa248d89460bcdee82861
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-09 15:59:14 +0000

    Add MathPi and Math.Pow methods

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,7 +6,7 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
-            double π = 3.1415926;
+            double π = Math.PI;
             double a;
             double r;
             double V;
@@ -15,7 +15,7 @@ namespace PerguntaValores
             a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("What is the radius of your cilinder?");
             r = Convert.ToDouble(Console.ReadLine());
-            V = π * (r*r) * a;
+            V = π * Math.Pow(r,r) * a;
             S = 2*π*r*(r+a);
             Console.WriteLine(V);
             Console.WriteLine(S);
