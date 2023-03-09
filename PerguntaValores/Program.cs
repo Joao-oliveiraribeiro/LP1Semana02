b9a57cafcd178c3d656b489e274c6cd7d1e30e6f
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-09 15:53:35 +0000

    Add surface area calculation

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -10,12 +10,15 @@ namespace PerguntaValores
             double a;
             double r;
             double V;
+            double S;
             Console.WriteLine("What is the height of your cilinder?");
             a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("What is the radius of your cilinder?");
             r = Convert.ToDouble(Console.ReadLine());
             V = π * (r*r) * a;
+            S = 2*π*r*(r+a);
             Console.WriteLine(V);
+            Console.WriteLine(S);
         }
     }
 }
