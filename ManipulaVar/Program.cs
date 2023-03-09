80154940ecae95d7c71e784a3431571da3d6e173
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-09 15:10:22 +0000

    Add read and convert variable arg to int and increment 1 value

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Console.WriteLine("Hello, World!");
+            int x= int.Parse(args[0]);
+            x++;
+            Console.WriteLine(x);
+        }
+    }
+}
