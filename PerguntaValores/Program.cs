534fbab350afae36fc065638b2a326dac03e4ab5
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-09 15:46:07 +0000

    Add height and radius question and formula of volume

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double π = 3.1415926;
+            double a;
+            double r;
+            double V;
+            Console.WriteLine("What is the height of your cilinder?");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the radius of your cilinder?");
+            r = Convert.ToDouble(Console.ReadLine());
+            V = π * (r*r) * a;
+            Console.WriteLine(V);
+        }
+    }
+}
