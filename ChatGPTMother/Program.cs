babb9601a076abd96e34d8a1c54359481b8d4392
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-10 11:44:33 +0000

    Add infinite cicle responses and cicle break

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // iNITIALIZING CICLE
+            while (true)
+            {
+                // Question detection
+                string question;
+                Console.WriteLine("What is your question?");
+                question = Console.ReadLine();
+                if (question != "EXIT")
+                {
+
+                    // Choose response
+                    string response = question switch
+                    {
+                        "What are you" => "Im a bot",
+                        "What do you do?" => "I respond to questions",
+                        "How do I stop you?" => "Write EXIT", 
+                        _ => "I m too dumb to recognize that question",
+                    };
+                    Console.WriteLine(response);
+                }
+                // Cicle break
+                if (question == "EXIT")
+                {
+                    break;
+                }
+            }        
+        }
+    }
+}
