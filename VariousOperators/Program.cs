6076dd1d26c92b574852bb38fed0fdccffb75d1f
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-26 14:35:01 +0000

    Write code for calculations and write on the console

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -6,9 +6,14 @@ namespace VariousOperators
     {
         private static void Main(string[] args)
         {
-            Console.Write("Insere número inteiro- não negativo: ");
+            Console.Write("Insere número inteiro-não negativo: ");
             string input = Console.ReadLine();
             byte n = byte.Parse(input);
+
+            Console.WriteLine(n / 2);
+            Console.WriteLine(n << 3);
+            Console.WriteLine(n ^ 6);
+            Console.WriteLine(n > 10);
         }
     }
 }
