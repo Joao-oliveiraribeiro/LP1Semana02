da6526b2f5babe20e1be33ba28f174dd7bba134a
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-26 14:29:11 +0000

    Write code for input number

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro- não negativo: ");
+            string input = Console.ReadLine();
+            byte n = byte.Parse(input);
+        }
+    }
+}
