21e18d18de69c874f30982a1908e006a99a91af8
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-26 14:20:08 +0000

    Write on console percentage values

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            string input = Console.ReadLine();
+            sbyte n = sbyte.Parse(input);
+
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+
+            
+        }
+    }
+}
