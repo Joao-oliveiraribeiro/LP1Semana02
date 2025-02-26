1102914d00efcde0524ad730d1275bbafb25f86b
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-26 14:41:58 +0000

    Write code for inputs by user and convert to double

---
+++ Cilindro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insira a altura do cilindro: ");
+            string input = Console.ReadLine();
+            double altura = double.Parse(input);
+
+            Console.Write("Insira o raio do cilindro: ");
+            string input2 = Console.ReadLine();
+            double raio = double.Parse(input2);
+
+            Console.Write("Insira o volume do cilindro: ");
+            string input3 = Console.ReadLine();
+            double volume = double.Parse(input3);
+
+            
+        }
+    }
+}
