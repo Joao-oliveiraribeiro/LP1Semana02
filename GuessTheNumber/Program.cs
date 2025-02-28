1f5f181bd17f5ffe078908a19d501eaf2f3b4d40
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-28 17:52:57 +0000

    Fix identation / format code

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -13,41 +13,41 @@ namespace GuessTheNumber
             bool cycle = true;
             int tries = 1;
 
-            while(cycle)
+            while (cycle)
             {
                 Console.Write("Insert number: ");
                 int input_number = int.Parse(Console.ReadLine());
 
-                if(numberToGuess == input_number)
+                if (numberToGuess == input_number)
                 {
                     Console.WriteLine($"You found the hidden number {input_number} after {tries} tries.");
                     cycle = false;
 
                 }
-                if(input_number < 0 || input_number >30)
-                    {
-                        Console.WriteLine("Numbers must be between 0 and 30. Try again.");
-                        tries++; 
-                        continue;
-                    }
-                if(numberToGuess > input_number)
-                    {
-                      Console.WriteLine($"The hidden number is higher than {input_number}. Try again.");
-                      tries++;
-                      continue;  
-                    }
-                if(numberToGuess < input_number)
-                    {
-                      Console.WriteLine($"The hidden number is lower than {input_number}. Try again.");
-                      tries++;
-                      continue;  
-                    }
-
-            
-
-                    
+                if (input_number < 0 || input_number > 30)
+                {
+                    Console.WriteLine("Numbers must be between 0 and 30. Try again.");
+                    tries++;
+                    continue;
                 }
+                if (numberToGuess > input_number)
+                {
+                    Console.WriteLine($"The hidden number is higher than {input_number}. Try again.");
+                    tries++;
+                    continue;
+                }
+                if (numberToGuess < input_number)
+                {
+                    Console.WriteLine($"The hidden number is lower than {input_number}. Try again.");
+                    tries++;
+                    continue;
+                }
+
+
+
+
             }
         }
     }
+}
 
