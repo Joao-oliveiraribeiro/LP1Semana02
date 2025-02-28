ef89ead7e9456e5254899c08e7f788128aa346de
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-02-28 13:20:31 +0000

    Add cycle and first question with reponse

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string question;
+            bool cycle = true;
+
+            while(cycle)
+            {
+                Console.WriteLine("En que posso ajudar?(Digite EXIT para sair)");
+                question = Console.ReadLine();
+
+                if (question == "EXIT")
+                {
+                    cycle = false;
+                }
+                else
+                {
+                    switch (question)
+                    {
+                        case "Como estás?":
+                        Console.WriteLine("Estou bem obrigado por perguntar");
+                        break
+
+                         
+                    }
+                }
+            }
+        }
+    }
+}
